b467e46ef8b2566d833fceaa02000d37847e7fb9
Maksym Lvov <83756212+TheMainOne@users.noreply.github.com>
2026-02-04 11:59:10 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/saleswidget">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
